6c7b06cc235f88b66392fe7231f47701a33dbc01
And4Web <anands2807@gmail.com>
2022-05-28 20:44:47 +0530

    reversing a string started

--- problem2.js
+++ problem2.js
@@ -1,2 +1,32 @@
 //problem2:
 //create an Array using class and constructor and implement the basic methods of arrays like push, pop, delete, shift etc.
+
+//this problem was demonstrated in section 6 lecture 5.
+
+//solution:
+
+class MyArray {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
+  }
+  pop() {
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+  delete(index) {}
+  shiftItem() {}
+}
+const newArray = new MyArray();
+newArray.push("anand");
+newArray.push("shukla");
+newArray.push("is");
+newArray.push("nice");
+newArray.pop();
+newArray.pop();
+console.log(newArray);
---
+++ problem3.js
@@ -0,0 +1,6 @@
+//problem3:
+//reverse a given string.
+//trick: whenever you have to do any action on strings, first convert that string into an array.
+//the above problem is demonstrated in section 6 lecture 7
+
+//solution:
