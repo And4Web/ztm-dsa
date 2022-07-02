5ec32c1fa58579ce1e66a22e3720273e04f3697c
And4Web <anands2807@gmail.com>
2022-07-02 12:22:05 +0530

    section 3 finished

--- section3/problem10.js
+++ section3/problem10.js
@@ -1,3 +1,5 @@
+//space compexity:
+
 function booo(n) {
   for (let i = 0; i < n.length; i++) {
     console.log("booo!");
@@ -11,7 +13,9 @@ function hiNTimes(n) {
     hiArray[i] = "hi!";
   }
   console.log(hiArray);
-  return hiArray;
+  // return hiArray;
 }
 
 hiNTimes(5);
+
+// here space compexity for the first function is O(1) as it is not creating any additional memory while loopint through, for the second function is O(n) as it is creating space in memory n times.
