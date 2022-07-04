c97eed4002439ab03ced36ae8368591d32eb1737
And4Web <anands2807@gmail.com>
2022-07-04 12:10:57 +0530

    created first hash table

---
+++ index.js
@@ -0,0 +1,57 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+    console.log("constructor: ", this.data);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+      // console.log(key[i], hash);
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+    console.log(this.data);
+    return this.data;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    const myBucket = this.data[address];
+
+    // console.log(myBucket);
+
+    if (myBucket) {
+      for (let i = 0; i < myBucket.length; i++) {
+        if (myBucket[i][0] === key) {
+          return myBucket[i][1];
+        }
+      }
+    }
+    return "no such item found in our Table.";
+  }
+
+  keys() {
+    //this method will help us to iterate through our keys in the list.
+  }
+}
+
+const myHashTable = new HashTable(20);
+// myHashTable._hash("grapes");
+myHashTable.set("grapes", 10000);
+myHashTable.set("mangoes", 5000);
+myHashTable.set("oranges", 15000);
+console.log(myHashTable.get("grapes"));
+console.log(myHashTable.get("mangoes"));
+console.log(myHashTable.get("mangoess"));
+// myHashTable.get("mangoes");
