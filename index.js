d2a9fb2963aa63ce8eaac6622f0f9a1394112aae
And4Web <anands2807@gmail.com>
2022-07-07 18:44:41 +0530

    prepend() method implemented

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  delete(value) {
+    // this.length--;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
@@ -26,6 +42,10 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 // myLinkedList.append(26);
+myLinkedList.prepend(36);
+myLinkedList.prepend(6);
 // myLinkedList.append(76);
+myLinkedList.delete(36);
 
 console.log("my linkedList: ", myLinkedList);
+console.log("head.next Object: ", myLinkedList.head.next);
