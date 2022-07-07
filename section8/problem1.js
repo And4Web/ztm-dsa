3ab0c510db6a7ebf43e392ac49918fa7681c5190
And4Web <anands2807@gmail.com>
2022-07-07 18:31:32 +0530

    created linked list and append()

---
+++ section8/problem1.js
@@ -0,0 +1,34 @@
+//first linkedList:
+
+//sample:
+/*
+10 -> 5 -> 16
+
+let linkedList = {
+  head: {
+    value: 10, 
+    next: {
+      value: 5,
+      next: {
+        value: 16, 
+        next: null
+      }
+    }
+  }
+}
+*/
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+
+console.log("my linkedList: ", myLinkedList);
